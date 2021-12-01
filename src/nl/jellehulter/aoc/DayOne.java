b4e8049c10b80dd87e8b9980d44f4b0f068f5e84
Jelle Hulter <jellehulter@gmail.com>
2021-12-01 08:44:30 +0100

    Added first day

--- src/nl/jellehulter/aoc/DayOne.java
+++ src/nl/jellehulter/aoc/DayOne.java
@@ -1,9 +1,54 @@
 package nl.jellehulter.aoc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DayOne {
 
-    public static void main(String[] args) {
+    public ArrayList<Integer> depths;
+    public ArrayList<Integer> threeDepths;
 
+    public DayOne() {
+        depths = new ArrayList<>();
+    }
+
+    public void compareDepths(ArrayList<Integer> list) {
+        int increases = 0;
+        for(int i = 1; i < list.size(); i++) {
+            if(list.get(i - 1) < list.get(i)) {
+                increases++;
+            }
+        }
+        System.out.println(increases);
+    }
+
+    public void calculateThreeDepths() {
+        threeDepths = new ArrayList<>();
+        for(int i = 2; i < depths.size(); i++) {
+            int x = depths.get(i-2) + depths.get(i-1) + depths.get(i);
+            System.out.println(x);
+            threeDepths.add(x);
+        }
+    }
+
+
+
+    public void readFile() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("src/nl/jellehulter/aoc/dayone.txt"));
+        while(scanner.hasNext()) {
+            depths.add(Integer.parseInt(scanner.nextLine().strip()));
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        DayOne dayOne = new DayOne();
+        dayOne.readFile();
+        dayOne.compareDepths(dayOne.depths);
+
+        dayOne.calculateThreeDepths();
+        dayOne.compareDepths(dayOne.threeDepths);
     }
 
 }
